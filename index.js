cd4d5cd385ead4430698bb99e15056e6221d1e0b
puran <puransapkota4@gmail.com>
2024-05-23 08:47:42 +1000

    create your own node module

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // in this code , we used the node js module http with create server feature. http itself is a function
-const fs = require("fs");
-const http = require("http");
-const os = require("os");
+// const fs = require("fs");
+// const http = require("http");
+// const os = require("os");
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        data: "Hello world!",
-      })
-    );
-  })
-  .listen(8000);
-console.log("Application is running");
+// http
+//   .createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "application/json" });
+//     res.end(
+//       JSON.stringify({
+//         data: "Hello world!",
+//       })
+//     );
+//   })
+//   .listen(8000);
+// console.log("Application is running");
 
 // http:// localhost:8000
 
@@ -21,10 +21,19 @@ console.log("Application is running");
 
 //fs use then show the content of hello.txt
 
-const result = fs.readFileSync("./hello.txt").toString;
-console.log({ result });
+// const result = fs.readFileSync("./hello.txt").toString();
+// console.log({ result });
 
 //os use and show total memory
 
-const mem = os.totalmem();
-console.log({ memory: mem });
+// const mem = os.totalmem();
+// console.log({ memory: mem });
+
+//Built in node js Module
+
+//crete your custom node js module
+
+const { converter } = require("./library");
+
+const result = converter(6000);
+console.log(result);
---
+++ library.js
@@ -0,0 +1,7 @@
+// write a function that conversts meter to kilometer
+
+const converter = (meter) => meter / 1000;
+
+module.exports = { converter };
+
+// write a js m
